01e116da5c66a38c748c81f5dec49c2c5df4ec59
Willian Pinheiro <willpinheiro@usp.br>
2022-06-22 12:39:33 -0300

    Remove terminal output and add HTTP response

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,12 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
+    let mut buffer: [u8; 1024] = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let response: &str = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
