542aa354bf9c00a397b47858a24005d0b49d9a72
Willian Pinheiro <willpinheiro@usp.br>
2022-06-22 22:44:37 -0300

    Web server respond asynchronously with thread pool

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,14 +1,18 @@
 use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use simple_web_server::ThreadPool;
 
 fn main() {
     let listener: TcpListener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream: TcpStream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
